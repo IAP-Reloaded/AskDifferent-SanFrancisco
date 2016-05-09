dd47853b8fd0ee76ba0d0a1eb2e2640c973a3d6a
Usernamer <Usernamer@users.noreply.github.com>
2016-05-09 16:41:55 -0400

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Ask Different in San Francisco
-// @version      0.2
+// @version      0.3
 // @description  changes default font of SE site Ask Different to San Francisco
 // @author       @IAP-Reloaded
 // @match        http*://*.apple.stackexchange.com/*
@@ -9,5 +9,5 @@
 
 var css = document.createElement("style");
 css.type = "text/css";
-css.innerHTML = "body { font-family: -apple-system, Helvetica Neue; }";
+css.innerHTML = "body { font-family: -apple-system, Helvetica Neue, Arial; }";
 document.body.appendChild(css);
